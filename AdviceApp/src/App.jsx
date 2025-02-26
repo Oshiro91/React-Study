626af791af0f492c041e0b62f4529b2d8700dd53
Oshiro91 <fernando.inagaki.oshiro@gmail.com>
2025-02-26 12:18:31 -0300

    getting advice API

--- AdviceApp/src/App.jsx
+++ AdviceApp/src/App.jsx
@@ -1,33 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [advice, setadvice] = useState("No Advice requested yet")
+
+  async function getAdvice() {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    setadvice(data.slip.advice);
+    setCount((c) => c+1 )
+  }
+
+  useEffect(function(){
+    getAdvice();
+  },[])
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <h1>{advice}</h1>
+        <button onClick={getAdvice}>Get Advice</button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          You have read {count} advices
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
