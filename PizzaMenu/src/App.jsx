e9b7d9b1807c0e18b7cdef349e31f577059c55c1
Oshiro91 <fernando.inagaki.oshiro@gmail.com>
2025-02-28 17:44:11 -0300

    creating components

--- PizzaMenu/src/App.jsx
+++ PizzaMenu/src/App.jsx
@@ -1,12 +1,48 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 
+function Header() {
+  return (
+    <h1>Fast React Pizza Co.</h1>
+  )
+}
+
+function Menu() {
+  return(
+    <div>
+      <h2>Our Menu</h2><Pizza/>
+    </div>
+  )
+}
+
+function Pizza(){
+  return(
+    <div>
+      <img src="/public/images/pizzas/spinaci.jpg" />
+      <h2>Pizza Spinacci</h2>
+      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
+    </div>
+  )
+}
+
+function Footer() {
+  return (
+    <footer>
+      <p><b>{new Date().toLocaleTimeString()}</b> We are currently open!</p>
+    </footer>
+  )
+
+}
+
+
 function App() {
 
   return (
     <>
       <div>
-        <h1>New Proj</h1>
+        <Header />
+        <Menu/>
+        <Footer />
       </div>
     </>
   )
