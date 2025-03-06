06f8341dfa801b1c92793f88bc3d467c8676999f
Oshiro91 <fernando.inagaki.oshiro@gmail.com>
2025-03-06 19:11:06 -0300

    Skelleton and some implements

--- PizzaMenu/src/App.jsx
+++ PizzaMenu/src/App.jsx
@@ -2,9 +2,49 @@ import { useEffect, useState } from 'react'
 import './App.css'
 import './index.css'
 
+function Avatar() {
+  return (
+    <div>
+      <img className='avatar' src="/images/pizzas/profile.jpg" alt="Fernando Oshiro" />
+    </div>
+  )
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Fernando Oshiro</h1>
+      <p>SAP consultant, especialized on MII module and UI5 front-end developer. Recently certified as a Developer integration SAP CPI</p>
+    </div>
+  )
+}
+
+function SkillList() {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+function Skill() {
+  return (
+    <div>
+      
+    </div>
+  )  
+}
 
 function App() {
-
+return(
+  <div className='card'>
+    <Avatar/>
+    <div className='data'>
+      <Intro/>
+      <SkillList/>
+    </div>
+  </div>
+)
   
 }
 
