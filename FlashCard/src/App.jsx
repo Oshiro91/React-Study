0e57f5a2bb38bdd21f6279186e832c4cf8bc4f88
Oshiro91 <fernando.inagaki.oshiro@gmail.com>
2025-04-11 10:51:41 -0300

    show&hide

--- FlashCard/src/App.jsx
+++ FlashCard/src/App.jsx
@@ -10,33 +10,40 @@ const messages = [
 function App() {
   const [step, setStep] = useState(1);
   const [buttonCollor, setButtonCollor] = useState({ backgroundColor: '#7950f2', color: '#fff' });
+  const [isOpen, setIsOpen] = useState(true);
 
   function handlePrevious() {
     if (step > 1) setStep(s => s - 1)
-      else alert("You can't go back")
+    else alert("You can't go back")
   }
 
   function handleNext() {
     if (step < 3) setStep(s => s + 1)
-      else alert("You can't go next")
+    else alert("You can't go next")
   }
 
-  
+
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={step >= 1 ? 'active' : 'innactive'}>1</div>
-        <div className={step >= 2 ? 'active' : 'innactive'}>2</div>
-        <div className={step >= 3 ? 'active' : 'innactive'}>3</div>
+    <div>
+      <button className="close" onClick={()=>setIsOpen(!isOpen)}>&times;</button>
+      {isOpen &&
+      <div className="steps">
+        <div className="numbers">
+          <div className={step >= 1 ? 'active' : 'innactive'}>1</div>
+          <div className={step >= 2 ? 'active' : 'innactive'}>2</div>
+          <div className={step >= 3 ? 'active' : 'innactive'}>3</div>
+        </div>
+        <p className="message">
+          Step {step}: {messages[step - 1]}
+        </p>
+        <div className="buttons">
+          <button style={buttonCollor} onClick={handlePrevious}>Previous</button>
+          <button style={buttonCollor} onClick={handleNext}>Next</button>
+        </div>
       </div>
-      <p className="message">
-        Step {step}: {messages[step - 1]}
-      </p>
-      <div className="buttons">
-        <button style={buttonCollor} onClick={handlePrevious}>Previous</button>
-        <button style={buttonCollor} onClick={handleNext}>Next</button>
-      </div>
+      }
     </div>
+
   )
 }
 
