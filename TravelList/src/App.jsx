ab513dfa0904d625b521c0e3d67216db7f7d8245
Oshiro91 <fernando.inagaki.oshiro@gmail.com>
2025-04-15 15:05:20 -0300

    progress

--- TravelList/src/App.jsx
+++ TravelList/src/App.jsx
@@ -13,11 +13,16 @@ function Logo() {
 
 function Form() {
   return (
-    <div className='add-form'>
+    <form className='add-form'>
       <h3>
         What do you need for your 😍 trip?
       </h3>
-    </div>
+      <select name="" id="">
+        {Array.from({length: 20}, (_, i) => i + 1).map(num => <option value={num} key={num}>{num}</option>)}
+      </select>
+      <input type="text" placeholder='Item...' />
+      <button>Add</button>
+    </form>
   )
 }
 
@@ -36,7 +41,7 @@ function PackingList() {
   return (
     <div className='list'>
       <ul>
-        {initialItems.map(item => <Item item={item} />)}
+        {initialItems.map(item => <Item item={item} key={item.id} />)}
       </ul>
     </div>
   )
