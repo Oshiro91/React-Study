42ac6840ff8faccd04f2d7773441a7d09dc0535f
Oshiro91 <fernando.inagaki.oshiro@gmail.com>
2025-04-15 11:43:07 -0300

    Starting the project

--- TravelList/src/App.jsx
+++ TravelList/src/App.jsx
@@ -1,11 +1,18 @@
 import './App.css'
 
+//initialItems
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "Charger", quantity: 1, packed: false },
+];
+
 function Logo() {
   return <h1>🌴 Far Away 💼</h1>
 }
 
 function Form() {
-  return(
+  return (
     <div className='add-form'>
       <h3>
         What do you need for your 😍 trip?
@@ -14,16 +21,29 @@ function Form() {
   )
 }
 
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  )
+}
+
 function PackingList() {
-  return(
+  return (
     <div className='list'>
-      List
+      <ul>
+        {initialItems.map(item => <Item item={item} />)}
+      </ul>
     </div>
   )
 }
 
 function Stats() {
-  return(
+  return (
     <footer className='stats'>
       <em>
         💼You have X items on your list, and you already packed X (X%)
@@ -33,12 +53,12 @@ function Stats() {
 }
 
 function App() {
-  return(
+  return (
     <div className='app'>
-      <Logo/>
-      <Form/>
-      <PackingList/>
-      <Stats/>
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
   )
 }
