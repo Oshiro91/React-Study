ccaef4143b249f8699dc309507afb7f95381fb51
Oshiro91 <fernando.inagaki.oshiro@gmail.com>
2025-04-17 11:23:56 -0300

    Stop

--- Accordion/src/App.jsx
+++ Accordion/src/App.jsx
@@ -2,11 +2,39 @@ import { useState } from 'react'
 import './App.css'
 import './index.css'
 
+const faqs = [
+  {
+    title: "Where are these chairs assembled?",
+    text:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus."
+  },
+  {
+    title: "How long do I have to return my chair?",
+    text:
+      "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus."
+  },
+  {
+    title: "Do you ship to countries outside the EU?",
+    text:
+      "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!"
+  }
+];
+
+
+function Accordion({number,title,text}) {
+  return (
+    <div className='item'>
+      <p className='number'>{number}</p>
+      <p className='title'>{title}</p>
+      <p className='icon'>{text}</p>
+    </div>
+  )
+}
 function App() {
 
   return (
     <div>
-      <h1>Hello World</h1>
+      <Accordion />
     </div>
   )
 }
