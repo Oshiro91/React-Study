658e6a430294c3d4300dcf9d9033fa1bdd585ab5
Oshiro91 <fernando.inagaki.oshiro@gmail.com>
2025-04-17 08:30:02 -0300

    Class 86

--- TravelList/src/App.jsx
+++ TravelList/src/App.jsx
@@ -57,10 +57,25 @@ function Item({ item, onDeleteItem, onToggleItem }) {
 }
 
 function PackingList({ items, onDeleteItem, onToggleItem }) {
+  const [sortBy, setSortBy] = useState("input");
+  let sortedItems;
+  switch (sortBy) {
+    case "input":
+      sortedItems = items;
+      break;
+    case "description":
+      sortedItems = items.slice().sort((a, b) => a.description.localeCompare(b.description));
+      break;
+    case "packed":
+      sortedItems = items.slice().sort((a, b) => Number(a.packed) - Number(b.packed));
+      break;
+    default:
+      break;
+  }
   return (
     <div className='list'>
       <ul>
-        {items.map(item => 
+        {sortedItems.map(item => 
         <Item 
         item={item} 
         onDeleteItem={onDeleteItem} 
@@ -68,6 +83,13 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
         onToggleItem={onToggleItem} 
         />)}
       </ul>
+      <div className='actions'>
+        <select value={sortBy} onChange={e => setSortBy(e.target.value)}>
+          <option value="input">Sort by input Order</option>
+          <option value="description">Sort by Description</option>
+          <option value="packed">Sort by Packed</option>
+        </select>
+      </div>
     </div>
   )
 }
