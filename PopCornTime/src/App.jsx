1d505e1a9ff2bc4d683ce2313b6d60d95a2b11a9
Oshiro91 <fernando.inagaki.oshiro@gmail.com>
2025-04-28 08:52:54 -0300

    Adjusting names and CSS

--- PopCornTime/src/App.jsx
+++ PopCornTime/src/App.jsx
@@ -23,6 +23,66 @@ const tempMovieData = [
     Poster:
       "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
   },
+  {
+    imdbID: "tt0468569",
+    Title: "The Dark Knight",
+    Year: "2008",
+    Poster: "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX300.jpg",
+  },
+  {
+    imdbID: "tt0816692",
+    Title: "Interstellar",
+    Year: "2014",
+    Poster: "https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
+  },
+  {
+    imdbID: "tt0110912",
+    Title: "Pulp Fiction",
+    Year: "1994",
+    Poster: "https://m.media-amazon.com/images/M/MV5BNGNhMDIzZTUtNTBlZi00MTRlLWFjM2ItYzViMjE3YzI5MjljXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
+  },
+  {
+    imdbID: "tt0109830",
+    Title: "Forrest Gump",
+    Year: "1994",
+    Poster: "https://m.media-amazon.com/images/M/MV5BNWIwODRlZTUtY2U3ZS00Yzg1LWJhNzYtMmZiYmEyNmU1NjMzXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+  },
+  {
+    imdbID: "tt0137523",
+    Title: "Fight Club",
+    Year: "1999",
+    Poster: "https://m.media-amazon.com/images/M/MV5BMmEzNTkxYjQtZTc0MC00YTVjLTg5ZTEtZWMwOWVlYzY0NWIwXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
+  },
+  {
+    imdbID: "tt0167260",
+    Title: "The Lord of the Rings: The Return of the King",
+    Year: "2003",
+    Poster: "https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
+  },
+  {
+    imdbID: "tt0114369",
+    Title: "Se7en",
+    Year: "1995",
+    Poster: "https://m.media-amazon.com/images/M/MV5BOTUwODM5MTctZjczMi00OTk4LTg3NWUtNmVhMTAzNTNjYjcyXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
+  },
+  {
+    imdbID: "tt0120737",
+    Title: "The Lord of the Rings: The Fellowship of the Ring",
+    Year: "2001",
+    Poster: "https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg",
+  },
+  {
+    imdbID: "tt0111161",
+    Title: "The Shawshank Redemption",
+    Year: "1994",
+    Poster: "https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
+  },
+  {
+    imdbID: "tt0068646",
+    Title: "The Godfather",
+    Year: "1972",
+    Poster: "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
+  }
 ];
 
 const tempWatchedData = [
@@ -46,6 +106,26 @@ const tempWatchedData = [
     imdbRating: 8.5,
     userRating: 9,
   },
+  {
+    imdbID: "tt0073195",
+    Title: "Rocky",
+    Year: "1976",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMTY5MDMzODU1OV5BMl5BanBnXkFtZTgwMjg0MjM0NjE@._V1_SX300.jpg",
+    runtime: 120,
+    imdbRating: 8.1,
+    userRating: 9,
+  },
+  {
+    imdbID: "tt0086190",
+    Title: "Star Wars: Episode VI - Return of the Jedi",
+    Year: "1983",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BOWZlMjFiYzgtMTUzNC00Y2IzLTk1NTMtZmNhMTczNTk0ODk1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg",
+    runtime: 131,
+    imdbRating: 8.4,
+    userRating: 10,
+  }
 ];
 
 function Navbar() {
@@ -90,27 +170,39 @@ function NumResults({ movies }) {
 
 function ListBox() {
   const [isOppen, setIsopen] = useState(true)
-  const [movies, setMovies] = useState(tempMovieData)
   return (
     <div className='box'>
       <button className='btn-toggle' onClick={() => setIsopen(!isOppen)}>{isOppen ? '-' : '+'}</button>
       {isOppen && (
-        <ul className='list'>
-          {movies.map((movie) => (
-            <li key={movie.imdbID}>
-              <img src={movie.Poster} alt={`${movie.Title} Poster`} />
-              <h3>{movie.Title}</h3>
-              <div>
-                <p>
-                  <span>📅</span>
-                  <span>{movie.Year}</span>
-                </p>
-              </div>
-            </li>
-          ))}
-        </ul>
+        <MovieList />
       )}
     </div>
+  )
+}
+
+function MovieList() {
+  const [movies, setMovies] = useState(tempMovieData)
+  return (
+    <ul className='list'>
+      {movies.map((movie) => (
+        <Movies key={movie.imdbID} movie={movie} />
+      ))}
+    </ul>
+  )
+}
+
+function Movies({ movie }) {
+  return (
+    <li key={movie.imdbID}>
+      <img src={movie.Poster} alt={`${movie.Title} Poster`} />
+      <h3>{movie.Title}</h3>
+      <div>
+        <p>
+          <span>📅</span>
+          <span>{movie.Year}</span>
+        </p>
+      </div>
+    </li>
   )
 }
 
@@ -127,55 +219,64 @@ function WatchedBox() {
 
   return (
     <div className='box'>
-      <button
-        className='btn-toggle'
-        onClick={() => setIsopen2(!isOppen2)}>
-        {isOppen2 ? '-' : '+'}
-      </button>
+      <div className='summary'>
+        <h2>Movies you watched
+          <button
+            className='btn-toggle'
+            onClick={() => setIsopen2(!isOppen2)}>
+            {isOppen2 ? '-' : '+'}
+          </button>
+        </h2>
+        <div>
+          <p>
+            <span>🎬</span>
+            <span>{watched.length}</span>
+            <span>🌟</span>
+            <span>{avgImdbRating}</span>
+            <span>🌠</span>
+            <span>{avgUserRating}</span>
+          </p>
+        </div>
+      </div>
       {isOppen2 && (
         <>
-          <div className='summary'>
-            <h2>Movies you watched</h2>
-            <div>
-              <p>
-                <span>🎬</span>
-                <span>{watched.length}</span>
-                <span>🌟</span>
-                <span>{avgImdbRating}</span>
-                <span>🌠</span>
-                <span>{avgUserRating}</span>
-              </p>
-            </div>
-          </div>
-
-          <ul className='list'>
-            {watched.map((movie) => (
-              <li key={movie.imdbID}>
-                <img src={movie.Poster} alt={`${movie.Title} Poster`} />
-                <h3>{movie.Title}</h3>
-                <div>
-                  <p>
-                    <span>⭐️</span>
-                    <span>{movie.imdbRating}</span>
-                  </p>
-                  <p>
-                    <span>🌟</span>
-                    <span>{movie.userRating}</span>
-                  </p>
-                  <p>
-                    <span>⏳</span>
-                    <span>{movie.runtime} min</span>
-                  </p>
-                </div>
-              </li>
-            ))}
-          </ul>
+          <WatchedList />
         </>
       )}
     </div>
   )
 }
 
+function WatchedList() {
+  const [watched, setWatched] = useState(tempWatchedData)
+  return (
+    <>
+      <ul className='list'>
+        {watched.map((movie) => (
+          <li key={movie.imdbID}>
+            <img src={movie.Poster} alt={`${movie.Title} Poster`} />
+            <h3>{movie.Title}</h3>
+            <div>
+              <p>
+                <span>⭐️</span>
+                <span>{movie.imdbRating}</span>
+              </p>
+              <p>
+                <span>🌟</span>
+                <span>{movie.userRating}</span>
+              </p>
+              <p>
+                <span>⏳</span>
+                <span>{movie.runtime} min</span>
+              </p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </>
+
+  )
+}
 function Main() {
   return (
     <main className='main'>
